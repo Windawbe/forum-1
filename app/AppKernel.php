98513943138dcb09cc731ed7132d01f560bcd1a6
Anthony Bocci <boccianthony@yahoo.fr>
2016-08-22 22:13:39 +0200

    Configuration de DForumBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,9 +17,9 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
-     //       new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
- //           new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
-   //         new Discutea\DForumBundle\DForumBundle(),
+           new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new Discutea\DForumBundle\DForumBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
